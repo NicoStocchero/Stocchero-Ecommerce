28843eee0c92c3939307fd7ed8cf32c842797461
Nicolás Stocchero <nikostocchero@gmail.com>
2025-06-02 14:06:23 -0300

    refactor(Loader): remove Loader component as it is no longer needed

--- src/components/ui/Loader.jsx
+++
@@ -1,7 +0,0 @@
-const Loader = () => (
-  <div className="flex justify-center items-center py-20">
-    <div className="w-12 h-12 border-4 border-orange-500 border-t-transparent rounded-full animate-spin"></div>
-  </div>
-);
-
-export default Loader;
